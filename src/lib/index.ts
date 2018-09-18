0ddb8bcdf63652a74e53c9bf600c7d282bb3b5a1
Tomoyuki Sato <bay@1101.com>
2018-09-18 20:46:42 +0900

    don't change opacity when fade option is false

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -147,6 +147,7 @@ const Lazyload:PluginObject<LazyLoadOptions> = {
             },
             update (el: ImageElement, binding: VNodeDirective) {
                 if (compareSrc(el.src,binding.value)) return
+                if (! fade) return
                 el.style.opacity = '0'
                 el.style.transition = `opacity ${time/2}ms`
                 el.newSrc = binding.value
